5dc67ee8fbc6e0328b938d0d3bb4ebc8e86b50e2
Tyler Dennis <ctdennis@protonmail.com>
2020-05-20 23:34:52 -0700

    Updated models.py DATABASE_URL/URI

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from app import app
 
 db = SQLAlchemy()
 
-database_path = "postgres://wdspwndlbhdcvj:d09f9e19edf9fb71d9879d37bde38c70415ba6a65a58dd695d3a7e075c2d1617@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d3ib0ohrcrh0lh"
+database_path = os.environ['DATABASE_URL']
 
 # setup_db(app)
 
